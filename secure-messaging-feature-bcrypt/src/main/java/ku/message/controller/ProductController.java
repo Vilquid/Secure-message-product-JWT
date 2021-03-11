b360e638922ab96447eedbc291131346ac5d60c4
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-11 11:46:59 +0700

    Update a ProductController class to GET an add product form and POST a product

--- secure-messaging-feature-bcrypt/src/main/java/ku/message/controller/ProductController.java
+++ secure-messaging-feature-bcrypt/src/main/java/ku/message/controller/ProductController.java
@@ -1,11 +1,14 @@
 package ku.message.controller;
 
+import ku.message.model.Product;
 import ku.message.service.JwtAccessTokenService;
 import ku.message.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,4 +25,18 @@ public class ProductController
 
 		return "product";
 	}
+
+	@GetMapping("/add")
+	public String getAddProductPage(Model model) {
+		return "product-add";
+	}
+
+	@PostMapping
+	public String createProduct(@ModelAttribute Product product, Model model)
+	{
+		productService.createProduct(product);
+
+		return "redirect:/product";
+	}
+
 }
