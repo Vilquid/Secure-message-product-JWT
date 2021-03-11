972858adfcd7d4271f1e0abf7cb22ab01a217b77
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-11 11:32:09 +0700

    Add a ProductService class
    Add a ProductService class to send requests to the Product APi with the Jwt token

---
+++ secure-messaging-feature-bcrypt/src/main/java/ku/message/service/ProductService.java
@@ -0,0 +1,36 @@
+package ku.message.service;
+
+import ku.message.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService
+{
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private JwtAccessTokenService tokenService;
+
+	public List<Product> getAllProducts()
+	{
+		String token = tokenService.requestAccessToken();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("authorization", "Bearer " + token);
+		HttpEntity entity = new HttpEntity(headers);
+
+		String url = "http://localhost:8091/api/products";
+
+		ResponseEntity<Product[]> response = restTemplate.exchange(url, HttpMethod.GET,	entity, Product[].class);
+
+		Product[] products = response.getBody();
+
+		return Arrays.asList(products);
+	}
+}
